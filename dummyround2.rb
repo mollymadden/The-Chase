d6a1309497e15aa0074e83b3d4756fbe29f8fa23
Molly Madden <molly.annette.madden@gmail.com>
2019-09-08 21:02:06 +1000

    more changes to chaser bot

---
+++ dummyround2.rb
@@ -0,0 +1,135 @@
+require 'tty-prompt'
+require 'tty-table'
+require 'ascii'
+require 'colorize'
+require 'json'
+
+@prompt = TTY::Prompt.new
+@Table = TTY::Table.new
+
+
+
+def round_2_intro
+
+    $chasers = ["The Beast", "The Shark", "The Governess", "Goliath", "The Supernerd"]
+    $your_chaser = $chasers.sample
+
+    $cash_build = 10000
+    $high_offer = $cash_build * 3
+    $low_offer = $cash_build / 4
+
+    $choice = ""
+
+    table = TTY::Table.new ["#{$your_chaser.colorize(:red)}",], [['---'], ["$#{$high_offer}"], ["$#{$cash_build}"], ["$#{$low_offer}"], ["---"], ["---"], ["---"], ["Bank".colorize(:yellow)]]
+    puts table.render(:ascii, alignments: [:center])
+
+    menu = @prompt.select("Your chaser today is #{$your_chaser}!! Would you like to play for your cash build of $#{$cash_build}, take the low offer of $#{$low_offer}, or take the high offer of $#{$high_offer}?", 
+    ["$#{$high_offer}", "$#{$cash_build}", "$#{$low_offer}"])
+
+      case menu
+          when "$#{$high_offer}"
+            table = TTY::Table.new ["#{$your_chaser.colorize(:red)}",], [['---'], ["$#{$high_offer}"], ['---'], ['---'], ["---"], ["---"], ["---"], ["Bank".colorize(:yellow)]]
+            puts table.render(:ascii, alignments: [:center])
+            $high_offer = $choice
+            $to_home = 6.to_i
+            puts "You'll need 6 correct answers to make it home."
+          when "$#{$cash_build}"
+            table = TTY::Table.new ["#{$your_chaser.colorize(:red)}",], [['---'], ['---'], ["$#{$cash_build}"], ['---'], ["---"], ["---"], ["---"], ["Bank".colorize(:yellow)]]
+            puts table.render(:ascii, alignments: [:center])
+            $cash_build = $choice
+            puts "You'll need 5 correct answers to make it home."
+            $to_home = 5.to_i
+          when "$#{$low_offer}"
+            table = TTY::Table.new ["#{$your_chaser.colorize(:red)}",], [['---'], ['---'], ['---'], ["$#{$low_offer}"], ["---"], ["---"], ["---"], ["Bank".colorize(:yellow)]]
+            puts table.render(:ascii, alignments: [:center])
+            $low_offer = $choice
+            puts "You'll need 4 correct answers to make it home."
+            $to_home = 4.to_i
+          end
+  end
+
+
+
+
+round_2_intro
+
+def get_questions_round_2
+  json_from_file = File.read("chase_round_2.json")
+  JSON.parse(json_from_file)['results']
+end
+
+def run_round_2
+  questions_arr = Array.new(get_questions_round_2)
+  score_count = $to_home;
+
+  while questions_arr.length > 0
+    curr_question = get_question(questions_arr)
+    answer_choices = get_answers(curr_question)
+
+    play(curr_question, answer_choices)
+
+    user_input = $selection
+    chaser_answer = correct_answer
+    chaser_score = 8
+
+    correct_answer = curr_question['correct_answer']
+        if user_input.downcase == correct_answer.downcase
+            puts "\nCorrect"
+            score_count -= 1
+            if score_count != 0
+            puts "You'll need #{score_count} more to make it home."
+            else puts "Nice job! You've made it!!"
+                system 'clear'
+            begin_round_3
+            end
+        else
+            puts "\nIncorrect"
+            puts "The correct answer is #{correct_answer}."
+            if chaser_answer == correct_answer
+            puts "\n#{$your_chaser} is hot on your tail!"
+            chaser_score -= 1
+                if chaser_score == score_count
+                puts "You've been caught! Better luck next time!"
+                # system 'clear'
+                else 
+                puts "How embarrasing for #{$your_chaser}"
+                end
+            end
+        end
+
+
+    delete_question(questions_arr, curr_question)
+
+
+  end
+
+  end
+
+
+
+def get_question(questions)
+  i = rand(questions.length)
+  question = questions[i]
+end
+
+def get_answers(question)  #combine correct and incorrect answer choices
+  choices = Array.new(question['incorrect_answers'])
+  i = rand(choices.length)
+  choices.insert(i, question['correct_answer'])
+end
+
+def play(question, answers)
+    answers.map { |answer| }
+    $selection = @prompt.select("#{question['question']}", answers)
+  end
+
+def delete_question(questions, question)
+  questions.delete(question)
+end
+
+run_round_2
+
+
+
+
+
